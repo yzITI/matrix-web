a64f5a3a2027915deee0e0f07bc71b1630b74130
Phantomlsh <phantomlsh@outlook.com>
2022-05-24 18:29:20 -0700

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -1,19 +1,19 @@
 const https = require('https'), crypto = require('crypto')
 
 const url = 'https://matrix.yzzx.org/srpc'
-let _name = '', _secret = '', data = []
+let _id = '', _secret = '', data = []
 
 const HS256 = () => crypto.createHmac('sha256', _secret).update(JSON.stringify(data)).digest('base64url')
 
-exports.setService = (name, secret) => {
-  _name = name
+exports.setService = (id, secret) => {
+  _id = id
   _secret = secret
 }
 
 function submit () {
   if (!data.length) return
   const req = request(url, { method: 'POST', headers: { 'Content-Type': 'application/json' } })
-  req.write(JSON.stringify({ N: 'log', A: [_name, data, HS256()] }))
+  req.write(JSON.stringify({ N: 'log', A: [_id, data, HS256()] }))
   data = []
   req.end()
 }
